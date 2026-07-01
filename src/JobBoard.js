7095f5707bf47edf29afa82d03f035c7b17cb81a
MPatefield <m.patefield95@hotmail.co.uk>
2026-07-01 14:21:31 +0100

    addition of multiple ways to write if/switch statement to return differeing values based on the job number at the present time

---
+++ src/JobBoard.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const JobBoard = () => {
+    const companyName = "Redacted";
+    const jobCount = 19;
+    const getJobMessage = () => {
+
+        //return jobCount === 0 ? "No jobs available" : `There are ${jobCount} jobs available at ${companyName}. There will be approximately ${Math.floor(jobCount * 1.5)} new jobs posted next week.`;
+        
+        /*if (jobCount === 0) {
+            return "No jobs available";
+        } else if (jobCount < 10){
+            return `There are plenty of jobs available at ${companyName}. There will be approximately ${Math.floor(jobCount * 1.5)} new jobs posted next week.`;
+        } else if (jobCount < 50) {
+            return `There are a moderate number of jobs available at ${companyName}. There will be approximately ${Math.floor(jobCount * 1.5)} new jobs posted next week.`;
+        */
+
+
+        switch (true) {
+            case jobCount === 0:
+                return "No jobs available";
+            case jobCount >=0 && jobCount < 10:
+                return `There are plenty of jobs available (${jobCount}) at ${companyName}. There will be approximately ${Math.floor(jobCount * 1.5)} new jobs posted next week.`;
+            case jobCount >= 10 && jobCount < 20:
+                return `There are a moderate number of jobs available (${jobCount}) at ${companyName}. There will be approximately ${Math.floor(jobCount * 1.5)} new jobs posted next week.`;
+            case jobCount >= 20 && jobCount < 50:
+                return `There are loads of jobs available (${jobCount}) at ${companyName}. There will be approximately ${Math.floor(jobCount * 1.5)} new jobs posted next week.`;
+        }
+    }
+  return (
+    <div>
+        <h1>{companyName} Job Board</h1>
+        <p>{getJobMessage()}</p>
+    </div>
+  )
+}
+
+export default JobBoard
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import CreateJob from './CreateJob';
+import JobBoard from './JobBoard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <CreateJob />
+    <JobBoard />
   </React.StrictMode>
 );
 
